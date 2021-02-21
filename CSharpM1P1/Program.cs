f056f4170d1ee0a2c8db64f212acfe268c484688
Lucas Garofalo <garofalodev@gmail.com>
2021-02-21 17:02:06 -0600

    First Commit

---
+++ CSharpM1P1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpM1P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i=1; i<=100; i++){
+            if(i % 3 == 0){
+                if(i % 5 == 0){
+                    Console.Write("FizzBuzz, ");
+                    }
+                else {Console.Write("Fizz, ");}
+                }
+            else {
+                if (i % 5 == 0){
+                    Console.Write("Buzz, ");
+                    }
+                else{
+                    Console.Write("{0}, ", i);
+                    }
+                }
+            }
+           Console.Read();
+        }
+    }
+}
